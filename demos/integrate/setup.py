4d4453c80e1ca7e083ce80b3194cac4947e8dc1a
Stefan van der Walt <stefan@sun.ac.za>
2012-04-22 18:19:01 -0700

    Also build integrate types.

--- demos/integrate/setup.py
+++ demos/integrate/setup.py
@@ -6,6 +6,7 @@ setup(
   cmdclass = {'build_ext': build_ext},
   ext_modules = [
     Extension("integrate_compiled", ["integrate.py"]),
+    Extension("integrate_types", ["integrate_types.pyx"]),
     Extension("integrate_cy", ["integrate_cy.pyx"]),
     Extension("integrate_any", ["integrate_any.pyx"])
   ]
