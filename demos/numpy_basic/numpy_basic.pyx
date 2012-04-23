77cfaf8182040e8704001ed69ede04b71cf2802c
Stefan van der Walt <stefan@sun.ac.za>
2012-04-22 18:19:34 -0700

    Minor cleanup of numpy_basic.

--- demos/numpy_basic/numpy_basic.pyx
+++ demos/numpy_basic/numpy_basic.pyx
@@ -1,8 +1,8 @@
-cimport numpy as np
+cimport numpy as cnp
 
 
-def foo(np.ndarray[np.float64_t, ndim=2] arr):
-    cdef int i, j
+def foo(cnp.ndarray[cnp.float64_t, ndim=2] arr):
+    cdef size_t i, j
     for i in range(arr.shape[0]):
         for j in range(arr.shape[1]):
             arr[i, j] = i + j
--- demos/numpy_basic/setup.py
+++ demos/numpy_basic/setup.py
@@ -6,8 +6,7 @@ import numpy
 setup(
     cmdclass = {'build_ext': build_ext},
     ext_modules = [
-        Extension("numpy_basic" ,
-                  ["numpy_basic.pyx"],
+        Extension("numpy_basic", ["numpy_basic.pyx"],
                   include_dirs=[numpy.get_include()],
         ),
 ])
