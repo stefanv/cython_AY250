2574e5037b26fd011a81fc3e801ca65d42bbe9c5
Stefan van der Walt <stefan@sun.ac.za>
2012-04-22 18:19:55 -0700

    Minor cleanup of parallel mul.

--- demos/parallel/prange_demo.pyx
+++ demos/parallel/prange_demo.pyx
@@ -8,25 +8,22 @@ from cython.parallel cimport prange
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
-def dot(cnp.ndarray[double, ndim=2, mode='c'] A,
-        cnp.ndarray[double, ndim=2, mode='c'] B,
-        cnp.ndarray[double, ndim=2, mode='c'] out):
+def dot(cnp.ndarray[cnp.float64_t, ndim=2] A,
+        cnp.ndarray[cnp.float64_t, ndim=2] B,
+        cnp.ndarray[cnp.float64_t, ndim=2] out):
 
     cdef:
         size_t rows_A, cols_A, rows_B, cols_B
         size_t i, j, k
-        double s
+        cnp.float64_t s
 
-    rows_A = A.shape[0]
-    cols_A = A.shape[1]
+    rows_A, cols_A = A.shape[0], A.shape[1]
+    rows_B, cols_B = B.shape[0], B.shape[1]
 
-    rows_B = B.shape[0]
-    cols_B = B.shape[1]
-
-    # Take each row of A
+    # Take each row in A
     for i in range(rows_A):
 
-        # And multiply by every column of B
+        # And multiply by every column in B
         for j in range(cols_B):
             s = 0
             for k in range(cols_A):
@@ -37,27 +34,24 @@ def dot(cnp.ndarray[double, ndim=2, mode='c'] A,
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
-def pdot(cnp.ndarray[double, ndim=2, mode='c'] A,
-         cnp.ndarray[double, ndim=2, mode='c'] B,
-         cnp.ndarray[double, ndim=2, mode='c'] out):
+def pdot(cnp.ndarray[cnp.float64_t, ndim=2] A,
+         cnp.ndarray[cnp.float64_t, ndim=2] B,
+         cnp.ndarray[cnp.float64_t, ndim=2] out):
 
     cdef:
         size_t rows_A, cols_A, rows_B, cols_B
         size_t i, j, k
         double s
 
-    rows_A = A.shape[0]
-    cols_A = A.shape[1]
-
-    rows_B = B.shape[0]
-    cols_B = B.shape[1]
+    rows_A, cols_A = A.shape[0], A.shape[1]
+    rows_B, cols_B = B.shape[0], B.shape[1]
 
     with nogil:
 
-        # Take each row of A
+        # Take each row in A
         for i in prange(rows_A):
 
-            # And multiply by every column of B
+            # And multiply by every column in B
             for j in range(cols_B):
                 s = 0
                 for k in range(cols_A):
